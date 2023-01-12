675bccb4ce12e407fe04b0b8650cc79452e4a685
Brahim Zerd <brahim.zerd@live.fr>
2023-01-12 22:33:46 +0100

    Intégration du nom + création class pour les données

--- front/src/pages/Profile.jsx
+++ front/src/pages/Profile.jsx
@@ -1,9 +1,38 @@
-
-
+import Header from '../components/Header'
+import Aside from '../components/Aside'
+import Bonjour from '../components/Bonjour'
+import { useParams } from 'react-router-dom'
+import getMockedDataById from '../data/getMockeddata'
+import { useEffect, useState } from 'react'
 
 
 function Profile() {
 
+    const [main, setMain] = useState({});
+    
+
+    
+    const {id} = useParams();
+        useEffect(() => {
+            getMockedDataById(id, setMain);
+        }, []);
+        
+        console.log(main)
+        
+
+return(
+    <div>
+        
+        
+        <Header />
+        <Bonjour 
+        userInfos = {main.userInfos}
+         />
+        
+        <Aside />
+    
+    </div>
+)
 
 }
 
